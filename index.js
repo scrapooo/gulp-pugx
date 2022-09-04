73e7410fb721e7b006de073870b1cfc789aef4c1
Hunter <1002237913@qq.com>
2022-09-04 13:42:51 +0800

    feat: 1) 支持gulp stream模式，2) 优化了部分代码

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 const transformPugx = require('./transform');
 const { Transform } = require('readable-stream');
+const { Readable } = require('node:stream');
+
+function streamToBuffer(stream) {
+  return new Promise((resolve, reject) => {
+    let buffers = [];
+    stream.on('error', reject);
+    stream.on('data', (data) => buffers.push(data));
+    stream.on('end', () => resolve(Buffer.concat(buffers)));
+  });
+}
 
 function gulpPugx(options) {
-  async function a(file, enc, callback) {
-    if (file.isNull()) {
-      return callback(null, file);
-    }
+  async function transform(file, enc, callback) {
     if (file.isStream()) {
-      return callback(null, file);
+      const rawCode = await streamToBuffer(file.contents).then((res) =>
+        res.toString('utf-8')
+      );
+      const transedCode = await transformPugx(rawCode, options);
+      file.contents = Readable.from(transedCode);
     }
     if (file.isBuffer()) {
-      const transdCode = await transformPugx(
-        file.contents.toString('utf-8'),
-        options
-      );
+      const rawCode = file.contents.toString('utf-8');
+      const transdCode = await transformPugx(rawCode, options);
       file.contents = Buffer.from(transdCode, 'utf-8');
     }
-    this.push(file);
     callback(null, file);
   }
 
   return new Transform({
     objectMode: true,
-    transform(file, enc, callback) {
-      a.call(this, file, enc, callback);
-    },
+    transform,
   });
 }
 
